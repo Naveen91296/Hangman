a92b0acea9676d4fc6b4d7966f2037b3199dbc14
Naveen <naveen.kumar@mewurk.com>
2025-05-23 08:11:27 +0530

    hangman drawing

--- src/Components/Hangman/index.tsx
+++ src/Components/Hangman/index.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
-import WORDS from "../../../public/data/hangmanWords.json";
+import WORDS from "../../data/hangmanWords.json";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 
 const Hangman = () => {
   const [wordToGuess, setWordToGuess] = useState<string>(
     WORDS[Math.floor(Math.random() * WORDS.length)]
   );
-  
-  return <div>Hangman {wordToGuess}</div>;
+
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "12px",
+      }}
+    >
+      <h3>Hangman header</h3>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 };
 
 export default Hangman;
